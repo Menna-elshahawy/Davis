65d4da3ab7048f8b843ddeb970f05a621686c232
unknown <shfouad@gmail.com>
2021-06-25 01:50:50 +0200

    user defined animations

--- app/Controllers/Http/ExecuteCodeController.js
+++ app/Controllers/Http/ExecuteCodeController.js
@@ -13,22 +13,24 @@ Axios.defaults.headers.post['Content-Type'] = 'application/json';
 class ExecuteCodeController {
 
 	
-	async executeCode1({request}){
+	async executeCode1({request,response}){
 		
 		var code=request.input('code');
 		var c=code.replace(/(\r\n|\n|\r)/gm, "");
-		console.log(c)
 
 	    await Axios.post('https://api.jdoodle.com/v1/execute',{
-	   		script: c,
-	        language: "python3",
-	        versionIndex: "0",
+			script: "  import java.util.Arrays; public class ArrayStack { private int[] theStack; private int maxSize; private int top; public ArrayStack(int s) { maxSize = s; theStack = new int[maxSize]; top = -1; } public void push(int elem) { top++; theStack[top] = elem; } public String printStack() { return (Arrays.toString(theStack)); } static void print(String []a) { System.out.println(Arrays.toString(a)); } public static void main(String[] args){ ArrayStack s=new ArrayStack(5); String[] a=new String[5]; a[0]=s.printStack(); s.push(2); a[1]=s.printStack(); s.push(6); a[2]=s.printStack(); s.push(5); a[3]=s.printStack(); s.push(9); a[4]=s.printStack(); s.push(3); print(a); s.printStack(); } }  ",
+	        language: "java",
+	        versionIndex: "3",
 	        clientId: "95c3d6d990faf6b1d2daf7ad0195b799",
 	        clientSecret: "3407587a0e94f79246c4682be471fafcb445a37ba99b06882ceb23ef203015bb"	
 	    })
-            .then(response => {
-        	    console.log(response.data.output);
-                View.global('res',response.data.output);
+            .then(Response => {
+				var result=Response.data.output
+        	    console.log("Result: " +String(result));
+				result=result
+                View.global('res',result);
+				return response.redirect('/UserDefined/stackRes');
             });
 	}
 
@@ -53,12 +55,15 @@ class ExecuteCodeController {
 				var result=Response.data.output;
 				
         	    // console.log("time:"+ timestamp + " in controller: "+ result);
-				console.log("Result: "+result)
 				View.global('res',result);
+				code=code+""
+				// View.global('code',String(code));
 				currentDate = new Date();
 				timestamp = currentDate.getTime() / 1000 ;
 				// console.log("time:"+ timestamp );
 				console.log("data: "+ data);
+				console.log("Result: "+result)
+				// data='priorityqueue'
 
 				if(data.localeCompare('stack')==0){
 					return response.redirect('/UserDefined/stackRes');
@@ -66,6 +71,10 @@ class ExecuteCodeController {
 
 				else if(data.localeCompare('queue')==0){
 					return response.redirect('/UserDefined/queueRes');
+				}
+
+				else if(data.localeCompare('priorityqueue')==0){
+					return response.redirect('/UserDefined/priorityqRes');
 				}
 
 				else if(data.localeCompare('LinkedList')==0){
